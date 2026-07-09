3b94eb0f442c2a100a8ed86e48479e604d35172f
het <hetpatel1684@gmail.com>
2026-07-09 14:02:34 +0530

    initial

--- src/p110.kt
+++ src/p110.kt
@@ -1,13 +1,16 @@
-class Car (var model:String){
+ open class Car (var model:String){
     var price:Double=0.0
     constructor(p: Double,m: String):this(m)
     {price=p
     }
 }
+ class Suzuki(m: String):Car(m){}
 fun main()
 {
     val c1=Car(12000.0,"alto")
     println("Model:${c1.model}")
     println("Price:${c1.price}")
-
+    val c2=Suzuki("BMW")
+    println("Model:${c2.model}")
+    println("Price:${c2.price}")
 }
